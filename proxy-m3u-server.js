0e77ff9d53efb25816f851998cd16e65528dc940
Jesusalz <jesus.uculture@gmail.com>
2024-11-06 11:17:20 -0300

    actualizado

--- proxy-m3u-server.js
+++ proxy-m3u-server.js
@@ -5,19 +5,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Proxy para servir el archivo M3U
-app.get('/proxy-m3u', (req, res) => {
+app.get('/proxy-m3u', async (req, res) => {
     try {
-        const filePath = path.join(__dirname, 'playlist.m3u'); // Asegúrate de que 'playlist.m3u' esté en la misma carpeta que tu archivo proxy-m3u-server.js
-        const m3uContent = fs.readFileSync(filePath, 'utf8');
-
-        // Puedes modificar el contenido del archivo M3U aquí si lo deseas
-        res.set('Content-Type', 'text/plain');
-
+        const response = await axios.get('URL_DE_TU_ARCHIVO_M3U');
+        const m3uContent = response.data;
+        // Puedes modificar `m3uContent` aquí, si deseas reescribir URLs o encriptar
+        res.set('Content-Type', 'text/plain');  // Cambiado a text/plain
         res.send(m3uContent);
     } catch (error) {
         res.status(500).send('Error al obtener el archivo M3U');
     }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Servidor de proxy corriendo en http://localhost:${PORT}`);
